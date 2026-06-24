71fc6ffaf909ca4fae9cd0cbfe345cafc0d53805
CLEMENT PETROD--LAPERRIERE <petrodlaperriere.clement@gmail.com>
2026-06-24 10:24:40 +0200

    adding function for data processing

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import numpy as np
 # import scipy as sc
 # import matplotlib.pyplot as plt
-# import glob
 import pathlib as pth
+import spectra as sp
 
-# from spectrum import spectrum
-
-lbwf_ini = np.array([[1605, 300, 83, -7, 1352, 200, 121, 0]])
+p0 = np.array([[1605, 5e-2, 83, -3e5, 1352, 3e-2, 121, 0]])
 path = pth.Path(pth.Path.cwd().anchor) / 'Users' / 'petrodlc' / 'Documents' / \
         'Data' / 'LEW87022' / 'exports' / 'LEW87022_405_01_Points0.txt'
 
@@ -24,17 +22,19 @@ for meteorite in [
     files[meteorite] = sorted(directory.glob(meteorite + '_405_*.txt'))
 
 
-def lorentz(w, w_max, fwhm, I_max):
-    return I_max * (fwhm / 2)**2 / ((w - w_max)**2 + (fwhm / 2)**2)
+def preprocess(s: sp.spectra, xoffset: float):
+    s.shift_x(520.7 - xoffset)
+    s.remove_bkg_poly(1100, 1900)
+    s.normalize()
+    return
 
 
-def bwf(w, w_max, Q, fwhm, I_max):
-    return I_max * (1 + (2 * (w - w_max) / (Q * fwhm))**2) / \
-            (1 + (2 * (w - w_max) / fwhm)**2)
-
-
-def lbwf(w, w_l, w_bwf, I_l, I_bwf, fwhm_l, fwhm_bwf, Q_bwf):
-    return lorentz(w, w_l, fwhm_l, I_l) + bwf(w, w_bwf, Q_bwf, fwhm_bwf, I_bwf)
+def compute_data(s: sp.spectra):
+    s.fit_lbwf(p0=p0, fit_range=[900, 2100])
+    s.compute_mean_data()
+    s.compute_mean_bkg_params()
+    s.compute_mean_fit_params()
+    return
 
 
 if __name__ == '__main__':
